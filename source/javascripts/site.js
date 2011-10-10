e92b5ce286467914e23ceb9cede995c1af0e17bc
Dustin Fadler <dfadler@gmail.com>
2011-10-10 08:59:02 -0500

    Need to figure out upward scrolling logic

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -2,7 +2,51 @@ $(function() {
 	
 	"use strict";
 	
-	var $this = this
+	var $this = this,
+		offset = []
+	
 
 		
+		// $(element)[0].scrollHeight
+		
+	$(window)
+		.bind('scroll', function(e, offset) {
+			// console.log($('body')[0].scrollHeight, $('body').height(), );
+			switch(e.type) {
+
+				case 'scroll':
+				
+
+					$('#blue')
+						.css({
+							'top': (($('#blue').offset().top + $(window).scrollTop()) / 5)
+						});
+					$('#yellow')
+						.css({
+							'top': (($('#yellow').offset().top + $(window).scrollTop()) / 7)
+						});
+					$('#brown')
+						.css({
+							'top': $(window).scrollTop()
+						});
+					$('#grey')
+						.css({
+							'top': (($('#grey').offset().top + $(window).scrollTop()) / 11)
+						});
+					$('#black')
+						.css({
+							'top': (($('#black').offset().top + $(window).scrollTop()) / 13)
+						});	
+						
+						console.log($('#brown').offset().top - $(window).scrollTop(), $('#brown').offset().top);
+					
+				default:				
+					break;
+			}
+			
+			
+		});	
+		
+		//$('body')[0].scrollHeight - $(window).scrollTop()
+console.log($('#brown').offset().top - $(window).scrollTop(), $('#brown').offset().top);
 });
